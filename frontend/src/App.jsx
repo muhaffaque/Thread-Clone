81dfe5344fb4b3691cac9856c527b634e0945ccc
muhaffaque <muhaffaquemufu007@gmail.com>
2023-09-13 09:53:46 +0530

    authentication done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,16 @@ import { Route, Routes } from "react-router-dom";
 import PostPage from "./pages/PostPage";
 import UserPage from "./pages/UserPage";
 import Header from "./components/Header";
+import HomePage from "./pages/HomePage";
+import AuthPage from "./pages/AuthPage";
 
 function App() {
   return (
     <Container maxW="620px">
       <Header/>
       <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/auth" element={<AuthPage/>}/>
         <Route path="/:username" element={<UserPage />} />
         <Route path="/:username/post/:pid" element={<PostPage/>} />
       </Routes>
---
+++ frontend/src/pages/AuthPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SignupCard from '../components/SignupCard'
+import LoginCard from '../components/LoginCard'
+
+const AuthPage = () => {
+  return (
+    <>
+      <LoginCard/>
+    </>
+  )
+}
+
+export default AuthPage
